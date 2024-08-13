e494bee0aee39e45412db86a2c8c81df6f2e6edc
roshansingh99 <roshan99singh87@gmail.com>
2024-08-13 14:37:50 +0000

    base

---
+++ base.js
@@ -0,0 +1,18 @@
+var a=10;
+
+console.log(b);
+
+if(a>10)
+{
+    console.log("a is greater");
+    
+}
+else if(a==0)
+{
+    console.log("ZERO");
+    
+}
+else{
+console.log("a is smaller");
+
+}
